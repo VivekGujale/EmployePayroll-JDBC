90e1240dd85b954470155549499ca0fc4feef006
VivekGujale <vdgujale007@.com>
2022-12-17 09:42:39 +0530

    Checking Database connection to payroll_service mysql DB is established or not

--- src/main/java/org/example/EmployeePayrollService/EmployeePayrollService.java
+++ src/main/java/org/example/EmployeePayrollService/EmployeePayrollService.java
@@ -1,10 +1,25 @@
 package org.example.EmployeePayrollService;
 
 
-import java.util.regex.Pattern;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class EmployeePayrollService {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Payroll DB");
+        String jdbcurl = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "********";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcurl, username, password);
+            connection.createStatement();
+            System.out.println("Database connection to payroll_service mysql DB is established.");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.getStackTrace();
+        }
     }
 }
