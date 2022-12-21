44d0b77167b4b3f335dee1bebdd54d7583b06da9
VivekGujale <vdgujale007@.com>
2022-12-21 21:49:25 +0530

    Added employee payroll service sql file

--- src/main/java/org/example/EmployeePayrollService/EmployeePayrollService.java
+++ src/main/java/org/example/EmployeePayrollService/EmployeePayrollService.java
@@ -8,23 +8,22 @@ public class EmployeePayrollService {
         System.out.println("Welcome to Employee Payroll DB");
         String jdbcurl = "jdbc:mysql://localhost:3306/payroll_service";
         String username = "root";
-        String password = "*******";
+        String password = "@Vacky_007";
         Connection connection;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcurl, username, password);
             String sql1 = "UPDATE employee_payroll SET basic_pay ='3000000' WHERE Name ='Terisa Reddy'";
+            //String sql2 = "SELECT SUM(salary) FROM employee_payroll WHERE gender = 'Male' GROUP BY gender";
             PreparedStatement preparedStatement = connection.prepareStatement(sql1);
+            //PreparedStatement preparedStatement1=connection.prepareStatement(sql2);
             preparedStatement.execute(sql1);
-            ResultSet resultSet = preparedStatement.executeQuery("SELECT * FROM employee_payroll");
-            System.out.println("EmployeeID\t\tName\t\tgender\t\tbasic_pay\tStartDate");
+            //preparedStatement1.execute(sql2);
+            ResultSet resultSet = preparedStatement.executeQuery("SELECT SUM(basic_pay) FROM employee_payroll WHERE gender = 'Male' GROUP BY gender");
+            System.out.println("\tbasic_pay");
             System.out.println("-------------------------------------------------------------");
             while (resultSet.next()) {
-                System.out.println(resultSet.getString(1) + "\t"
-                        + resultSet.getString(2) + "\t"
-                        + resultSet.getString(3) + "\t\t"
-                        + resultSet.getString(4) + "\t\t"
-                        + resultSet.getString(5));
+                System.out.println(resultSet.getString(""));
             }
             connection.commit();
             connection.close();
